a87d6036c91c5264fb6e60b202e0dbd91f061fce
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2018-04-17 21:16:07 +0800

    Java Edition of formula

---
+++ com/hhs/xgn/rating/type/Rating.java
@@ -0,0 +1,10 @@
+package com.hhs.xgn.rating.type;
+
+import java.util.*;
+import java.util.Map.Entry;
+public class Rating {
+	public String username;
+	public List<Contest> score=new ArrayList<Contest>();
+	public List<Contribution> con=new ArrayList<Contribution>();
+	
+}
